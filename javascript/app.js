8dde77ab3378d5f4c86f32e536518bee11c97e97
obadanaser <>
2021-05-08 00:43:06 +0000

    editing some css

--- javascript/app.js
+++ javascript/app.js
@@ -42,7 +42,7 @@ var favSocMedia = prompt("what is your favourite social media ?");
 
 
 
-var times = prompt('how many times do you use ' + favSocMedia + '?');
+var times = prompt('how many times do you use ' + favSocMedia + ' in a day ?');
 document.write('<h3>' + 'This is how many times you use ' + favSocMedia + ' ' + ':' + ' ' + times + '</h3>');
 
 
@@ -51,17 +51,22 @@ for (var i = 0; i < times; i++) {
 
   if (favSocMedia == "facebook") {
 
-    document.write('<img id="facebook" src="https://www.facebook.com/images/fb_icon_325x325.png" width=15%" > ')
+    document.write('<a href="https://web.facebook.com/?_rdc=1&_rdr"><img id="facebook" src="https://www.facebook.com/images/fb_icon_325x325.png" width=7%" ></a> ')
 
   }
   else if (favSocMedia == "youtube") {
 
-    document.write('<img id="youtube "src="https://yt3.ggpht.com/ytc/AAUvwnjDwLJeWs_jcgoVvQpC7YZxWMwP-N__UH-98dxGyw=s900-c-k-c0x00ffffff-no-rj" width=15%"     > ');
+    document.write('<a href="https://www.youtube.com/"><img id="youtube "src="https://yt3.ggpht.com/ytc/AAUvwnjDwLJeWs_jcgoVvQpC7YZxWMwP-N__UH-98dxGyw=s900-c-k-c0x00ffffff-no-rj" width=10%" ></a> ');
 
   }
   else if (favSocMedia == "instagram") {
 
-    document.write('<img src="https://nveloop.com/wp-content/uploads/2021/02/instagram.jpg" width=15%"  >')
+    document.write('<a href="https://www.instagram.com/"><img src="https://nveloop.com/wp-content/uploads/2021/02/instagram.jpg" width=10%" ></a>')
+
+  }
+  else if (favSocMedia == "whatsapp") {
+
+    document.write('<a href="https://www.whatsapp.com/"><img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" width=10%" ></a>')
 
   }
 
